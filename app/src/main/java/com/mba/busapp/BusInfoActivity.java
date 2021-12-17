8643d1bcad49cfa1fc5115eff460c24b6e68bad9
BEMELON <hgdkim2@naver.com>
2021-12-17 17:35:43 +0900

    Init project

---
+++ app/src/main/java/com/mba/busapp/BusInfoActivity.java
@@ -0,0 +1,6 @@
+package com.mba.busapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BusInfoActivity  extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/mba/busapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.mba.busapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.mba.busapp.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
