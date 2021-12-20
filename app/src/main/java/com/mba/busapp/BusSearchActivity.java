5e001f3da6f7d04f07aecaa9f846281896a8aebc
BEMELON <hgdkim2@naver.com>
2021-12-21 00:29:04 +0900

    [Class] BusSearchActivity

--- app/src/main/java/com/mba/busapp/BusSearchActivity.java
+++ app/src/main/java/com/mba/busapp/BusSearchActivity.java
@@ -1,6 +1,36 @@
 package com.mba.busapp;
 
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BusSearchActivity  extends AppCompatActivity {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+
+public class BusSearchActivity  extends AppCompatActivity implements OnMapReadyCallback {
+    private NaverMapManager mapManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bussearch);
+
+        // 네이버맵 Listener 연결
+        MapView mapView = findViewById(R.id.bussearch_navermap);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        mapManager = new NaverMapManager(naverMap, this);
+
+        mapManager.setCameraPosition(new LatLng(37.233972549267705, 127.18874893910944));
+        mapManager.enableLocationButton();
+        // mapManager.enableLocationButton(); 문제있네...
+
+    }
 }
