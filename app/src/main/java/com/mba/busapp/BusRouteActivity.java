ea6fff7565bc8318e3515ab2d2bca3d9b095c6e0
BEMELON <hgdkim2@naver.com>
2021-12-27 18:23:01 +0900

    [CLASS] BusRouteActivity   - 3번 레이아웃 구현   - 4번 레이아웃 구현   - 기타 필요한 파 업로드

--- app/src/main/java/com/mba/busapp/BusRouteActivity.java
+++ app/src/main/java/com/mba/busapp/BusRouteActivity.java
@@ -1,6 +1,9 @@
 package com.mba.busapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -10,12 +13,14 @@ import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 /**
  * 버스 노선도 화면
  */
 public class BusRouteActivity extends AppCompatActivity implements OnMapReadyCallback {
     private NaverMapManager mapManager;
+    private SlidingUpPanelLayout slidingUpPanelLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +31,9 @@ public class BusRouteActivity extends AppCompatActivity implements OnMapReadyCal
         MapView mapView = findViewById(R.id.busroute_navermap);
         mapView.getMapAsync(this);
 
+        // sliding panel 설정
+        slidingUpPanelLayout = (SlidingUpPanelLayout) findViewById(R.id.busroute_slidingpanel);
+        slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
     }
 
     @Override
@@ -34,5 +42,15 @@ public class BusRouteActivity extends AppCompatActivity implements OnMapReadyCal
         mapManager.setCameraPosition(new LatLng(37.233972549267705, 127.18874893910944), 15);
         // mapManager.enableLocationButton(); 문제있네...
 
+    }
+
+    public void btn_newActivity(View v) {
+        Log.d("[Button]", "netActivity Clicked!");
+        startActivity(new Intent(getApplicationContext(), BusTableView.class));
+    }
+
+    public void btn_slidingView(View v) {
+        Log.d("[Button]", "slidingView Clicked!");
+        slidingUpPanelLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
     }
 }
