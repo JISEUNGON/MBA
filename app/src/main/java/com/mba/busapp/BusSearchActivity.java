25e565e55426ac629dc23dfeef511a4d47411d93
sungxsoo <njs783075@gmail.com>
2022-01-04 03:06:47 +0900

    [CLASS] BusSearchActivity - 검색 UI 상단 창 작성 - switch 버튼 클릭 시 (textView, spinner) 위치 변경

--- app/src/main/java/com/mba/busapp/BusSearchActivity.java
+++ app/src/main/java/com/mba/busapp/BusSearchActivity.java
@@ -3,10 +3,14 @@ package com.mba.busapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.MapView;
@@ -15,11 +19,17 @@ import com.naver.maps.map.OnMapReadyCallback;
 
 public class BusSearchActivity  extends AppCompatActivity implements OnMapReadyCallback {
     private NaverMapManager mapManager;
+    private int switch_counter;
+    private Spinner spinner;
+    private TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bussearch);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        textView = (TextView) findViewById(R.id.textView);
 
         // 네이버맵 Listener 연결
         MapView mapView = findViewById(R.id.bussearch_navermap);
@@ -49,5 +59,12 @@ public class BusSearchActivity  extends AppCompatActivity implements OnMapReadyC
      */
     public void btn_moveNext(View v) {
         startActivity(new Intent(getApplicationContext(), BusResultActivity.class));
+    }
+
+    public void btn_switch(View v) {
+        ConstraintLayout.LayoutParams params1 = (ConstraintLayout.LayoutParams)spinner.getLayoutParams();
+        ConstraintLayout.LayoutParams params2 = (ConstraintLayout.LayoutParams)textView.getLayoutParams();
+        textView.setLayoutParams(params1);
+        spinner.setLayoutParams(params2);
     }
 }
